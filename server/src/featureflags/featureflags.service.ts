3781ec3a0935069646c9751ec7c147b2c21139c4
Illia Kliuiev <i.kliuiev@easygenerator.com>
2022-01-20 13:40:05 +0200

    feat(server): ff service mock basic functions

--- server/src/featureflags/featureflags.service.ts
+++ server/src/featureflags/featureflags.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+const mockFlags: { [key: string]: boolean } = {};
 
 @Injectable()
-export class FeatureFlagsService {}
+export class FeatureFlagsService {
+  checkFlag(flagName: string): boolean {
+    if (typeof mockFlags[flagName] === 'undefined')
+      throw new NotFoundException();
+
+    return mockFlags[flagName];
+  }
+
+  setFlag(flagName: string, value: boolean) {
+    mockFlags[flagName] = value;
+  }
+
+  deleteFlag(flagName: string) {
+    if (typeof mockFlags[flagName] === 'undefined')
+      throw new NotFoundException();
+
+    delete mockFlags[flagName];
+  }
+}
