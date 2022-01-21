4a84a7e61c8c0618cdea2e50cfb30f3a120b094c
Illia Kliuiev <i.kliuiev@easygenerator.com>
2022-01-21 17:50:38 +0200

    fix(server): startup dependency issues

--- server/src/instances/instances.module.ts
+++ server/src/instances/instances.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { InstancesService } from './instances.service';
 import { InstancesController } from './instances.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   providers: [InstancesService],
-  controllers: [InstancesController]
+  controllers: [InstancesController],
 })
 export class InstancesModule {}
