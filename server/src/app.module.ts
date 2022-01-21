6374de4e18c7824e858714a72c7efde62e8eb3af
Illia Kliuiev <i.kliuiev@easygenerator.com>
2022-01-21 17:54:46 +0200

    chore(server): remove unused

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { FeatureFlagsModule } from './featureflags/featureflags.module';
 import { DatabaseModule } from './database/database.module';
 import { InstancesModule } from './instances/instances.module';
 
 @Module({
   imports: [FeatureFlagsModule, DatabaseModule, InstancesModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
