63151ae970a519dd572eb5d99fe6b07f8081ddb2
Illia Kliuiev <i.kliuiev@easygenerator.com>
2022-01-24 10:46:29 +0200

    fix(server): nest dependencies

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { DatabaseService } from 'src/database/database.service';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  imports: [DatabaseService],
+  imports: [DatabaseModule],
   providers: [UsersService],
   controllers: [UsersController],
 })
