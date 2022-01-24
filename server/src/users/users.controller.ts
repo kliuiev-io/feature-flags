1950a523a7c5c29a48f1d6647087f7cee19cb2cf
Illia Kliuiev <i.kliuiev@easygenerator.com>
2022-01-24 10:59:28 +0200

    feat(server): add missing user flags routes

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -8,6 +8,7 @@ import {
   Put,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import FlagsDto from 'src/groups/dto/flags.dto';
 import GroupsDto from './dto/groups.dto';
 import RegisterUserDto from './dto/register-user.dto';
 import { UsersService } from './users.service';
@@ -53,5 +54,22 @@ export class UsersController {
     @Body() groupsDto: GroupsDto,
   ) {
     return this.usersService.setGroups(email, instance, groupsDto.groups);
+  }
+
+  @Get(':instance/:email/flags')
+  async getFlags(
+    @Param('instance') instance: string,
+    @Param('email') email: string,
+  ) {
+    return this.usersService.getFlags(email, instance);
+  }
+
+  @Put(':instance/:email/flags')
+  async setFlags(
+    @Param('instance') instance: string,
+    @Param('email') email: string,
+    @Body() flagsDto: FlagsDto,
+  ) {
+    return this.usersService.setGroups(email, instance, flagsDto.flags);
   }
 }
