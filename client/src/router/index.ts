b91fa86aea32b8743bc78aaef13111e63cccc779
Illia Kliuiev <i.kliuiev@easygenerator.com>
2022-01-27 06:25:01 +0200

    feat(client): use hash history to speedup

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 import Instances from '@/views/instances/Instances.vue';
 import Groups from '@/views/groups/Groups.vue';
 import Users from '@/views/users/Users.vue';
@@ -27,7 +27,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
 });
 
